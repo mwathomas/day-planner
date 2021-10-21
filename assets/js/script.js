d63b96073f2250e991d2653a6a70a453f347a412
M Abrams <madeline.a.thomas@gmail.com>
2021-10-21 18:30:46 -0400

    added README file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,47 +3,47 @@ var workday = [
   {
     hour: "09",
     time: "09",
-    ampm: "am",
+    ampm: "am ",
   },
   {
     hour: "10",
     time: "10",
-    ampm: "am",
+    ampm: "am ",
   },
   {
     hour: "11",
     time: "11",
-    ampm: "am",
+    ampm: "am ",
   },
   {
     hour: "12",
     time: "12",
-    ampm: "pm",
+    ampm: "pm ",
   },
   {
     hour: "01",
     time: "13",
-    ampm: "pm",
+    ampm: "pm ",
   },
   {
     hour: "02",
     time: "14",
-    ampm: "pm",
+    ampm: "pm ",
   },
   {
     hour: "03",
     time: "15",
-    ampm: "pm",
+    ampm: "pm ",
   },
   {
     hour: "04",
     time: "16",
-    ampm: "pm",
+    ampm: "pm ",
   },
   {
     hour: "05",
     time: "17",
-    ampm: "pm",
+    ampm: "pm ",
   },
 ];
 
@@ -69,21 +69,26 @@ workday.forEach(function (blockTime) {
   var hourBox = $("<div>").attr({
     class: "col-md-9 description p-0",
   });
-  var eventInfo = $("<textarea>");
+  var eventInfo = $("<textarea>").attr({
+    class: "event",
+  });
   hourBox.append(eventInfo);
 
   //colors the hour blocks based on current time
   if (blockTime.time < moment().format("HH")) {
     eventInfo.attr({
       class: "past",
+      id: "event",
     });
   } else if (blockTime.time === moment().format("HH")) {
     eventInfo.attr({
       class: "present",
+      id: "event",
     });
   } else if (blockTime.time > moment().format("HH")) {
     eventInfo.attr({
       class: "future",
+      id: "event",
     });
   }
 
@@ -98,44 +103,10 @@ workday.forEach(function (blockTime) {
   rows.append(hourField, hourBox, saveEvent);
 });
 
-// // loads any existing localstorage data after components created
-// init();
-
+var saveBtnClick = document.querySelector(".saveBtn");
+var eventInput = document.querySelector(".event");
 // // saves data to be used in localStorage
-// $(".saveBtn").on("click", function (event) {
-//   event.preventDefault();
-//   var saveIndex = $(this)
-//     .siblings(".description")
-//     .children(".future")
-//     .attr("id");
-//   myDay[saveIndex].reminder = $(this)
-//     .siblings(".description")
-//     .children(".future")
-//     .val();
-//   console.log(saveIndex);
-//   saveReminders();
-//   displayReminders();
-// });
-
-// // saves data to localStorage
-// function saveEvent() {
-//   localStorage.setItem("workday", JSON.stringify(workday));
-// }
-
-// // sets any data in localStorage to the view
-// function displayReminders() {
-//   workday.forEach(function (_thisHour) {
-//     $(`#${_thisHour.id}`).val(_thisHour.reminder);
-//   });
-// }
-
-// // sets any existing localStorage data to the view if it exists
-// function init() {
-//   var storedDay = JSON.parse(localStorage.getItem("workday"));
-
-//   if (storedDay) {
-//     myDay = storedDay;
-//   }
-
-//   saveEvent();
-//   displayReminders();
+saveBtnClick.addEventListener("click", function (event) {
+  event.preventDefault();
+  localStorage.setItem("event", eventInput.value);
+});
